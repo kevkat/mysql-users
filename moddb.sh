297cf8826f0bce51ef64d389a956c07dcb01e094
kevkat <kevkat@gmail.com>
2013-06-21 16:13:59 -0300

    Update moddb.sh

--- moddb.sh
+++ moddb.sh
@@ -5,6 +5,9 @@ EXPECTED_ARGS=3
 E_BADARGS=65
 MYSQL=`which mysql`
 
+# Q1 and Q2 are really the only differences
+# Q1 revokes all permissions, giving us a clean slate
+# Q2 grants just the user premissions
 Q1="REVOKE ALL PRIVILEGES ON *.* FROM '$2'@'%';"
 Q2="GRANT ALL ON ${BTICK}$1${BTICK}.* TO '$2'@'%' IDENTIFIED BY '$3';"
 Q3="FLUSH PRIVILEGES;"

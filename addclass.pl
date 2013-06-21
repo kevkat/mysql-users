1771711869b955cc84caf758a36c681c868f9562
kevkat <kevkat@gmail.com>
2013-06-21 16:07:04 -0300

    Update addclass.pl

--- addclass.pl
+++ addclass.pl
@@ -1,14 +1,21 @@
 #!/usr/bin/perl
 
-$password = 'smgdatabase';
+# This script takes input from a list file with usernames separated by carriage returns.
+# Each line will invoke crearedb, another script, to create a database and user matching
+# the username in the file.
+# e.g.: ./addclass my_class_list
+# ./createdb can be used separately for one-off user adds
 
-if (@ARGV) {
+$password = 'smgdatabase'; # everyone gets the same default password, smgdatabase
+
+if (@ARGV) { # While the file has lines, iterate through the while loop
   while (<>) {
-    chomp;
-    system("sh createdb $_ $_ $password");
+    chomp; # Cleans up trailing whitespace and line break characters
+    system("sh createdb $_ $_ $password"); # Passes this stuff to the command line
   }
 }
 
-else {
+# The script needs an argument to be run, otherwise it gives you directions
+else { 
   print STDERR "usage: ./addclass listfile\n";
 }
